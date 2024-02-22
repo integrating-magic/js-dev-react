77927b1563b973f9e8ce0c00fb9b3ad3f06f560d
Jeremy Brown <jlbmagic@gmail.com>
2024-02-22 11:46:35 -0500

    boothupdate

---
+++ src/BoothDiagram.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { DiagramComponent } from "@syncfusion/ej2-react-diagrams";
+
+const BoothDiagram = () => {
+  return <DiagramComponent id="container" width={"100%"} height={"350px"} />;
+};
+
+export default BoothDiagram;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import MyApp from "./myApp";
+import BoothDiagram from "./BoothDiagram";
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH9fd3VWQmRZVkZ+X0c="
+);
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<MyApp />);
+window.loadApp = () => {
+  root.render(<BoothDiagram />);
+};
