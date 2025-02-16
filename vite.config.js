2fdbe575d901822e804522908104629dcbba61cd
integrating-magic <jeremy@integratingmagic.io>
2025-02-16 07:56:27 -0500

    updated to vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 1234,
+  },
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: "esnext",
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+      //   output: {
+      //     manualChunks: () => "everything.js",
+      //   },
+    },
+  },
+});
